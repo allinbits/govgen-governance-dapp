979fd19ca9179983238d3d15db73fd69bf6b133e
stuyk <trevorwessel@protonmail.com>
2024-01-26 15:05:10 -0700

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+# GovGen Governance dApp
+
+An application for viewing proposals, voting on proposals, and viewing proposal statistics.
+
+## ⚙️ Development
+
+Install latest version `pnpm` and restart your terminal if necessary.
+
+### Install
+
+Install all packages in the repository.
+
+```
+pnpm i
+```
+
+### Dev
+
+```
+pnpm dev
+```
+
+### Build
+
+Recursively build all packages in the monorepo.
+
+```
+pnpm build
+```
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+let world = ref('World')
+</script>
+
+<template>
+  <div>
+    <h1><div class="world" /></h1>
+    <h2>Hello {{ world }}</h2>
+  </div>
+</template>
+
+<style scoped>
+@keyframes WorldEmoji {
+  0% {
+    content: '\01F30D';
+  }
+  33% {
+    content: '\01F30E';
+  }
+  66% {
+    content: '\01F30F';
+  }
+}
+
+.world::before {
+  content: '\01F30D';
+}
+
+.world::before {
+  animation: WorldEmoji 2s linear infinite alternate;
+}
+</style>
