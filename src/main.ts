c3670a249d389b9d10a317469e9eac06aa092eb6
stuyk <trevorwessel@protonmail.com>
2024-02-09 07:43:12 -0700

    feat: giscus integration

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
+import Giscus from "@giscus/vue";
 
 const app = createApp(App);
 app.use(router);
+app.component("GiscusWidget", Giscus);
 app.mount("#app");
