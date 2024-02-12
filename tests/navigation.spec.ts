f8762e65c649c483b809a97bfb5d81bee1fbe2c8
stuyk <trevorwessel@protonmail.com>
2024-02-12 13:17:39 -0700

    fix: playwright tests, and add serve as dev dep

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("test navigation", async ({ page }) => {
-  await page.goto("/", { timeout: 30000 });
-  await expect(page.locator("a")).toHaveText("Proposals");
-  await expect(page.locator("a")).toHaveText("Guides");
+  await page.goto("http://localhost:8080/", { timeout: 30000 });
+  await expect(page.getByRole('navigation')).toContainText('Proposals');
+  await expect(page.getByRole('navigation')).toContainText('Guides');
 });
