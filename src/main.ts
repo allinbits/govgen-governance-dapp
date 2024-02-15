c0a4f6d2e1f97baf04d87aabbb5ffb66fbee5591
Stuyk <trevorwessel@protonmail.com>
2024-02-15 14:43:36 -0700

    feat: pass custom discussion names to single widget component (#8)

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 import Giscus from "@giscus/vue";
+import GiscusCommentsComponent from "./components/GiscusComments.vue";
 
 const app = createApp(App);
 app.use(router);
 app.component("GiscusWidget", Giscus);
+app.component("GiscusComments", GiscusCommentsComponent);
 app.mount("#app");
