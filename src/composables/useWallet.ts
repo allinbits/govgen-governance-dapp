6e97ac6a619f21a1cb9d4535b36b05974b5f0211
Alex M. - Clockwork <alex@clockwork.gr>
2024-02-22 14:55:58 +0200

    feat: add sendTx

--- src/composables/useWallet.ts
+++ src/composables/useWallet.ts
@@ -1,12 +1,16 @@
-import { ref, computed } from "vue";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ref, computed, Ref } from "vue";
 import chainInfo from "../chain-config.json";
+import { EncodeObject, OfflineSigner } from "@cosmjs/proto-signing";
+import { SigningStargateClient } from "@cosmjs/stargate";
+import { getOfflineSigner } from "@cosmostation/cosmos-client";
 
 export enum Wallets {
   keplr = "KEPLR",
   leap = "LEAP",
   cosmostation = "COSMOSTATION",
 }
-export const useWallet = () => {
+const useWalletInstance = () => {
   const walletState = {
     keplr: computed(() => !!window.keplr),
     leap: computed(() => !!window.leap),
@@ -20,7 +24,7 @@ export const useWallet = () => {
     walletState.used.value = null;
     walletState.loggedIn.value = false;
   };
-
+  const signer: Ref<OfflineSigner | null> = ref(null);
   const connect = async (walletType: Wallets) => {
     switch (walletType) {
       case Wallets.keplr:
@@ -30,8 +34,8 @@ export const useWallet = () => {
           walletState.address.value = (await window.getOfflineSigner(chainInfo.chainId).getAccounts())[0].address;
           walletState.loggedIn.value = true;
           walletState.used.value = Wallets.keplr;
+          signer.value = window.getOfflineSigner(chainInfo.chainId);
         }
-
         break;
       case Wallets.leap:
         await window.leap?.experimentalSuggestChain(chainInfo);
@@ -39,6 +43,7 @@ export const useWallet = () => {
         walletState.address.value = (await window.leap.getOfflineSigner(chainInfo.chainId).getAccounts())[0].address;
         walletState.loggedIn.value = true;
         walletState.used.value = Wallets.leap;
+        signer.value = window.leap.getOfflineSigner(chainInfo.chainId);
         break;
       case Wallets.cosmostation:
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -55,17 +60,39 @@ export const useWallet = () => {
             coinType: "" + chainInfo.bip44.coinType, // optional
           },
         });
-        walletState.address.value = // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          (
-            await (window.cosmostation as any).cosmos.request({
-              method: "cos_requestAccount",
-              params: { chainName: "cosmoshub-4" },
-            })
-          ).address;
+        walletState.address.value = (
+          await (window.cosmostation as any).cosmos.request({
+            method: "cos_requestAccount",
+            params: { chainName: "cosmoshub-4" },
+          })
+        ).address;
         walletState.loggedIn.value = true;
         walletState.used.value = Wallets.cosmostation;
+        signer.value = getOfflineSigner(chainInfo.chainId);
         break;
     }
   };
-  return { ...walletState, signOut, connect };
+  const sendTx = async (msgs: EncodeObject[]) => {
+    if (signer.value) {
+      try {
+        const client = await SigningStargateClient.connectWithSigner(chainInfo.rpc, signer.value);
+        const result = await client.signAndBroadcast(walletState.address.value, msgs, fee);
+        return result;
+      } catch (e) {
+        throw new Error("Could not sign messages");
+      }
+    } else {
+      throw new Error("No Signer available");
+    }
+  };
+  return { ...walletState, signOut, connect, sendTx };
+};
+
+let walletInstance: ReturnType<typeof useWalletInstance>;
+
+export const useWallet = () => {
+  if (!walletInstance) {
+    walletInstance = useWalletInstance();
+  }
+  return walletInstance;
 };
