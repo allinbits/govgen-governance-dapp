8486d9459c20b95cd6fc7633297dab128829d641
Alex M. - Clockwork <alex@clockwork.gr>
2024-02-22 16:27:33 +0200

    feat: Polishing + address changes

--- src/composables/useWallet.ts
+++ src/composables/useWallet.ts
@@ -28,47 +28,61 @@ const useWalletInstance = () => {
   const connect = async (walletType: Wallets) => {
     switch (walletType) {
       case Wallets.keplr:
-        await window.keplr?.experimentalSuggestChain(chainInfo);
-        await window.keplr?.enable(chainInfo.chainId);
-        if (window.getOfflineSigner) {
-          walletState.address.value = (await window.getOfflineSigner(chainInfo.chainId).getAccounts())[0].address;
-          walletState.loggedIn.value = true;
-          walletState.used.value = Wallets.keplr;
-          signer.value = window.getOfflineSigner(chainInfo.chainId);
+        try {
+          await window.keplr?.experimentalSuggestChain(chainInfo);
+          await window.keplr?.enable(chainInfo.chainId);
+          if (window.getOfflineSigner) {
+            walletState.address.value = (await window.getOfflineSigner(chainInfo.chainId).getAccounts())[0].address;
+            walletState.loggedIn.value = true;
+            walletState.used.value = Wallets.keplr;
+            signer.value = window.getOfflineSigner(chainInfo.chainId);
+          } else {
+            throw new Error("Could not connect to Keplr: getOfflineSigner method does not exist");
+          }
+        } catch (e) {
+          throw new Error("Could not connect to Keplr: " + e);
         }
         break;
       case Wallets.leap:
-        await window.leap?.experimentalSuggestChain(chainInfo);
-        await window.leap?.enable(chainInfo.chainId);
-        walletState.address.value = (await window.leap.getOfflineSigner(chainInfo.chainId).getAccounts())[0].address;
-        walletState.loggedIn.value = true;
-        walletState.used.value = Wallets.leap;
-        signer.value = window.leap.getOfflineSigner(chainInfo.chainId);
+        try {
+          await window.leap?.experimentalSuggestChain(chainInfo);
+          await window.leap?.enable(chainInfo.chainId);
+          walletState.address.value = (await window.leap.getOfflineSigner(chainInfo.chainId).getAccounts())[0].address;
+          walletState.loggedIn.value = true;
+          walletState.used.value = Wallets.leap;
+          signer.value = window.leap.getOfflineSigner(chainInfo.chainId);
+        } catch (e) {
+          throw new Error("Could not connect to Leap Wallet: " + e);
+        }
         break;
       case Wallets.cosmostation:
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        await (window.cosmostation as any).cosmos.request({
-          method: "cos_addChain",
-          params: {
-            chainId: chainInfo.chainId,
-            chainName: chainInfo.chainName,
-            addressPrefix: chainInfo.bech32Config.bech32PrefixAccAddr,
-            baseDenom: chainInfo.stakeCurrency.coinMinimalDenom,
-            displayDenom: chainInfo.stakeCurrency.coinDenom,
-            restURL: chainInfo.rest,
-            decimals: chainInfo.stakeCurrency.coinDecimals, // optional
-            coinType: "" + chainInfo.bip44.coinType, // optional
-          },
-        });
-        walletState.address.value = (
+        try {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
           await (window.cosmostation as any).cosmos.request({
-            method: "cos_requestAccount",
-            params: { chainName: "cosmoshub-4" },
-          })
-        ).address;
-        walletState.loggedIn.value = true;
-        walletState.used.value = Wallets.cosmostation;
-        signer.value = getOfflineSigner(chainInfo.chainId);
+            method: "cos_addChain",
+            params: {
+              chainId: chainInfo.chainId,
+              chainName: chainInfo.chainName,
+              addressPrefix: chainInfo.bech32Config.bech32PrefixAccAddr,
+              baseDenom: chainInfo.stakeCurrency.coinMinimalDenom,
+              displayDenom: chainInfo.stakeCurrency.coinDenom,
+              restURL: chainInfo.rest,
+              decimals: chainInfo.stakeCurrency.coinDecimals, // optional
+              coinType: "" + chainInfo.bip44.coinType, // optional
+            },
+          });
+          walletState.address.value = (
+            await (window.cosmostation as any).cosmos.request({
+              method: "cos_requestAccount",
+              params: { chainName: "cosmoshub-4" },
+            })
+          ).address;
+          walletState.loggedIn.value = true;
+          walletState.used.value = Wallets.cosmostation;
+          signer.value = (await getOfflineSigner(chainInfo.chainId)) as OfflineSigner; // Appears to be a re-declaration of OfflineSigner and TS cannot figure out they're the same.
+        } catch (e) {
+          throw new Error("Could not connect to Cosmostation: " + e);
+        }
         break;
     }
   };
@@ -76,7 +90,10 @@ const useWalletInstance = () => {
     if (signer.value) {
       try {
         const client = await SigningStargateClient.connectWithSigner(chainInfo.rpc, signer.value);
-        const result = await client.signAndBroadcast(walletState.address.value, msgs, fee);
+        const result = await client.signAndBroadcast(walletState.address.value, msgs, {
+          amount: [{ amount: "0", denom: chainInfo.feeCurrencies[0].coinMinimalDenom }],
+          gas: "200000",
+        });
         return result;
       } catch (e) {
         throw new Error("Could not sign messages");
@@ -85,6 +102,15 @@ const useWalletInstance = () => {
       throw new Error("No Signer available");
     }
   };
+  const refreshAddress = () => {
+    if (walletState.used.value) {
+      connect(walletState.used.value);
+    }
+  };
+  window.addEventListener("cosmostation_keystorechange", refreshAddress);
+  window.addEventListener("keplr_keystorechange", refreshAddress);
+  window.addEventListener("leap_keystorechange", refreshAddress);
+
   return { ...walletState, signOut, connect, sendTx };
 };
 
