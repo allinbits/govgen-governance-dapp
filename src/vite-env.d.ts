d505d9297b113b13d4426e92b33e031e6c315d49
Clockwork <alex@clockwork.gr>
2024-02-23 13:29:14 +0200

    feat: Wallet composable (#11)
    * feat: Basic Initial setup of wallet composable
    
    * feat: add sendTx
    
    * chore: add required cosmjs polyfills
    
    * feat: Polishing + address changes
    
    * fix: Correct chain id
    
    * chore: Also lint JSON

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,15 @@
 /// <reference types="vite/client" />
+import type { Window as KeplrWindow, Keplr } from "@keplr-wallet/types";
 
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>;
   export default component;
+}
+declare global {
+  interface Window extends KeplrWindow {
+    leap: Keplr;
+    cosmostation: unknown;
+  }
 }
