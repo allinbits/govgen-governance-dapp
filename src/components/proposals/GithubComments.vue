18dd745e6712b85b80bf2f630bc8758e2d97b377
stuyk <trevorwessel@protonmail.com>
2024-03-13 13:46:40 -0600

    fix: remove extra style tags

--- src/components/proposals/GithubComments.vue
+++ src/components/proposals/GithubComments.vue
@@ -147,24 +147,3 @@ onMounted(refresh);
     </div>
   </div>
 </template>
-
-<style>
-.loader {
-  border: 4px solid #f3f3f3; /* Light grey */
-  border-top: 4px solid #3498db; /* Blue */
-  border-radius: 50%;
-  width: 50px;
-  height: 50px;
-  animation: spin 1s linear infinite;
-  margin: 20px auto; /* Center the spinner */
-}
-
-@keyframes spin {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-</style>
