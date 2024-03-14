8246565b87820eea9ce7162201b5deab774fd443
Clockwork <alex@clockwork.gr>
2024-03-14 21:04:38 +0200

    feat: Proposal page Header (#27)
    * wip
    
    * wip
    
    * feat: Basic Proposal Query & TX
    
    * feat: Allow loading *.graphql files
    
    * feat: Additional queries and txs
    
    * feat: Add VoteHistory query
    
    * feat: Weighted Votes
    
    * feat: add tally results query
    
    * feat: add proposal tallies getter
    
    * wip: codegen
    
    * feat: Add typesafe codegen
    
    * feat: Various fixes & updates
    
    * fix: linting
    
    * feat: Proposal page header and fixes
    
    * fix: remove unnecessary data render
    
    * feat: Move graphql endpoitns to .env
    
    * fix: don't break build on unused proposalTallies
    
    * fix: remove unused
    
    * feat: Add all tailwind `w-` classes to build since we will be using them dynamically
    
    * feat: add relative time formatting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,6 +44,10 @@ export default <Config>{
         500: "#0D0D0D",
       },
       button: "rgba(255,255,255,0.05)",
+      badge: {
+        info: "rgba(107, 239, 255, 0.10)",
+        plain: "rgba(255, 255, 255, 0.10)",
+      },
       accent: {
         100: "#6BEFFF",
         200: "#F4AFFF",
@@ -93,6 +97,11 @@ export default <Config>{
       900: [`${pxToRem(56)}rem`, { lineHeight: `${pxToRem(64)}rem` }],
     },
   },
+  safelist: [
+    {
+      pattern: /w-+/,
+    },
+  ],
   plugins: [
     plugin(function ({ addUtilities, addComponents }) {
       addUtilities({
