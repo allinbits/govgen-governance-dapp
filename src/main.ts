ef173fe4f64ee03a48f031333e30ded1443983cc
Clockwork <alex@clockwork.gr>
2024-03-20 09:48:05 +0200

    feat: Adjust env

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import { messages } from "./localization";
 // HTTP connection to the API
 const httpLink = createHttpLink({
   // You should use an absolute URL here
-  uri: import.meta.env.VITE_GRAPHQL_ENDPOINT ?? "http://164.92.143.81:8080/v1/graphql",
+  uri: import.meta.env.VITE_GRAPHQL_ENDPOINT ?? "https://graphql.devnet.govgen.dev/v1/graphql",
 });
 
 // Cache implementation
