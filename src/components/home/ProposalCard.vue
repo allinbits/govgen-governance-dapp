18b91bb4ae8c161571fa691ec9fd9c4bff16eba5
stuyk <trevorwessel@protonmail.com>
2024-03-22 10:27:10 -0600

    fix: spacing of text on larger proposal cards

--- src/components/home/ProposalCard.vue
+++ src/components/home/ProposalCard.vue
@@ -3,13 +3,13 @@ const { link } = defineProps<{ link: string }>();
 </script>
 
 <template>
-  <router-link :to="link" class="flex flex-col gap-14 px-8 py-6 bg-grey-300 rounded hover:bg-grey-200">
+  <router-link :to="link" class="flex flex-col gap-14 px-8 py-6 bg-grey-300 rounded hover:bg-grey-20">
     <div class="flex flex-row justify-end text-100">
       <slot name="header"></slot>
     </div>
-    <div class="flex flex-col text-grey-100 text-300 font-medium">
+    <div class="flex flex-col text-grey-100 text-300 font-medium flex-grow">
       <slot name="number"></slot>
-      <div class="flex flex-col text-[white] text-500 font-semibold text-left">
+      <div class="flex flex-col text-[white] text-500 font-semibold">
         <slot></slot>
       </div>
     </div>
