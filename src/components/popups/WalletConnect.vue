bf77734b38f7764128a1f84b7cb15f139840ef5f
alexiscolin <alexis@jaunebleu.co>
2024-04-02 16:05:50 +0200

    style: wallet transition

--- src/components/popups/WalletConnect.vue
+++ src/components/popups/WalletConnect.vue
@@ -75,104 +75,106 @@ bus.on("open", () => {
     </template>
 
     <!-- Wallet Selection -->
-    <template v-if="selectState">
-      <div class="absolute right-0 top-4 z-10">
-        <div class="flex flex-col gap-6 px-8 py-6 bg-grey-300 rounded w-80 relative">
-          <Icon class="absolute top-3 right-4 cursor-pointer text-light" icon="close" @click="isOpen = false" />
-          <div class="flex flex-col text-[white] text-500 font-semibold text-center">Connect your wallet</div>
-          <div class="flex flex-col text-grey-100 text-300 font-medium text-center">
-            You need to connect your wallet first to vote on proposals.
-          </div>
-          <div class="buttons">
-            <ConnectButton class="my-4" :disabled="!keplr" @click="connectWallet(Wallets.keplr)">
-              <template #icon>
-                <Icon icon="keplr" :size="1.4" class="mr-2" />
-              </template>
-              Keplr Wallet</ConnectButton
-            >
-            <ConnectButton class="my-4" :disabled="!leap" @click="connectWallet(Wallets.leap)"
-              ><Icon icon="leap" :size="1.1" class="mr-2 bg-dark p-1 rounded-sm" />Leap Wallet</ConnectButton
-            >
-            <ConnectButton class="my-4" :disabled="!cosmostation" @click="connectWallet(Wallets.cosmostation)">
-              <template #icon> <Icon icon="cosmostation" :size="1.1" class="mr-2 bg-dark p-1 rounded-sm" /> </template
-              >Cosmostation Wallet</ConnectButton
-            >
-          </div>
-        </div>
-      </div>
-    </template>
-
-    <!-- Normal signed in account display -->
-    <template v-if="connectedState">
-      <div class="flex align-center items-stretch cursor-pointer" @click="isOpen = true">
-        <div class="bg-gradient w-10 h-10 rounded-full mr-3"></div>
-        <div class="flex flex-col justify-around">
-          <div class="text-light text-200">{{ shorten(address) }}</div>
-          <div class="text-100 text-grey-100"><UserBalance :address="address" :denom="'ugovgen'" /> govgen</div>
-        </div>
-      </div>
-    </template>
-
-    <!-- Normal signed in account extended -->
-    <template v-if="viewState">
-      <div class="absolute right-0 top-4">
-        <div class="flex flex-col px-8 py-4 pt-12 bg-grey-300 rounded w-80 relative">
-          <Icon class="absolute top-3 right-4 cursor-pointer text-light" icon="close" @click="isOpen = false" />
-          <div class="flex align-center items-stretch">
-            <div class="bg-gradient w-10 h-10 rounded-full mr-3"></div>
-            <div class="flex flex-col justify-around">
-              <div class="text-light text-300">{{ shorten(address) }}</div>
+    <Transition name="fade">
+      <template v-if="selectState">
+        <div class="absolute right-0 top-4 z-10">
+          <div class="flex flex-col gap-6 px-8 py-6 bg-grey-300 rounded w-80 relative">
+            <Icon class="absolute top-3 right-4 cursor-pointer text-light" icon="close" @click="isOpen = false" />
+            <div class="flex flex-col text-[white] text-500 font-semibold text-center">Connect your wallet</div>
+            <div class="flex flex-col text-grey-100 text-300 font-medium text-center">
+              You need to connect your wallet first to vote on proposals.
+            </div>
+            <div class="buttons">
+              <ConnectButton class="my-4" :disabled="!keplr" @click="connectWallet(Wallets.keplr)">
+                <template #icon>
+                  <Icon icon="keplr" :size="1.4" class="mr-2" />
+                </template>
+                Keplr Wallet</ConnectButton
+              >
+              <ConnectButton class="my-4" :disabled="!leap" @click="connectWallet(Wallets.leap)"
+                ><Icon icon="leap" :size="1.1" class="mr-2 bg-dark p-1 rounded-sm" />Leap Wallet</ConnectButton
+              >
+              <ConnectButton class="my-4" :disabled="!cosmostation" @click="connectWallet(Wallets.cosmostation)">
+                <template #icon> <Icon icon="cosmostation" :size="1.1" class="mr-2 bg-dark p-1 rounded-sm" /> </template
+                >Cosmostation Wallet</ConnectButton
+              >
             </div>
           </div>
-          <div class="text-200 text-grey-100 pt-6 pb-2">Balance</div>
-          <div class="text-300 text-light"><UserBalance :address="address" :denom="'ugovgen'" /> govgen</div>
-          <div class="buttons">
-            <ConnectButton
-              class="my-4 justify-center"
-              @click="
-                signOut();
-                isOpen = false;
-              "
-            >
-              Disconnect Wallet</ConnectButton
-            >
+        </div>
+      </template>
+
+      <!-- Normal signed in account display -->
+      <template v-else-if="connectedState">
+        <div class="flex align-center items-stretch cursor-pointer" @click="isOpen = true">
+          <div class="bg-gradient w-10 h-10 rounded-full mr-3"></div>
+          <div class="flex flex-col justify-around">
+            <div class="text-light text-200">{{ shorten(address) }}</div>
+            <div class="text-100 text-grey-100"><UserBalance :address="address" :denom="'ugovgen'" /> govgen</div>
           </div>
         </div>
-      </div>
-    </template>
+      </template>
 
-    <!-- Connection in progress -->
-    <template v-if="connectingState">
-      <div class="absolute right-0 top-4">
-        <div class="flex flex-col px-8 py-4 pt-6 bg-grey-300 rounded w-80 relative align-center items-center">
-          <Icon icon="loading" :size="3" />
-
-          <div class="flex flex-col text-[white] text-400 font-semibold text-center mt-4">Connecting Wallet</div>
-          <div class="text-200 text-grey-100 my-4">Please wait...</div>
-          <div class="buttons">
-            <ConnectButton
-              class="my-4 justify-center"
-              @click="
-                () => {
-                  cancelConnect();
-                }
-              "
-            >
-              Cancel</ConnectButton
-            >
-          </div>
-
-          <div v-if="isSlowConnecting">
-            <a
-              :href="getWalletHelp(chosenWallet)"
-              target="_blank"
-              class="text-100 flex my-2 justify-center items-center"
-              >{{ chosenWallet }} Troubleshooting <Icon icon="link" class="ml-2"
-            /></a>
+      <!-- Normal signed in account extended -->
+      <template v-else-if="viewState">
+        <div class="absolute right-0 top-4">
+          <div class="flex flex-col px-8 py-4 pt-12 bg-grey-300 rounded w-80 relative">
+            <Icon class="absolute top-3 right-4 cursor-pointer text-light" icon="close" @click="isOpen = false" />
+            <div class="flex align-center items-stretch">
+              <div class="bg-gradient w-10 h-10 rounded-full mr-3"></div>
+              <div class="flex flex-col justify-around">
+                <div class="text-light text-300">{{ shorten(address) }}</div>
+              </div>
+            </div>
+            <div class="text-200 text-grey-100 pt-6 pb-2">Balance</div>
+            <div class="text-300 text-light"><UserBalance :address="address" :denom="'ugovgen'" /> govgen</div>
+            <div class="buttons">
+              <ConnectButton
+                class="my-4 justify-center"
+                @click="
+                  signOut();
+                  isOpen = false;
+                "
+              >
+                Disconnect Wallet</ConnectButton
+              >
+            </div>
           </div>
         </div>
-      </div>
-    </template>
+      </template>
+
+      <!-- Connection in progress -->
+      <template v-else-if="connectingState">
+        <div class="absolute right-0 top-4">
+          <div class="flex flex-col px-8 py-4 pt-6 bg-grey-300 rounded w-80 relative align-center items-center">
+            <Icon icon="loading" :size="3" />
+
+            <div class="flex flex-col text-[white] text-400 font-semibold text-center mt-4">Connecting Wallet</div>
+            <div class="text-200 text-grey-100 my-4">Please wait...</div>
+            <div class="buttons">
+              <ConnectButton
+                class="my-4 justify-center"
+                @click="
+                  () => {
+                    cancelConnect();
+                  }
+                "
+              >
+                Cancel</ConnectButton
+              >
+            </div>
+
+            <div v-if="isSlowConnecting">
+              <a
+                :href="getWalletHelp(chosenWallet)"
+                target="_blank"
+                class="text-100 flex my-2 justify-center items-center"
+                >{{ chosenWallet }} Troubleshooting <Icon icon="link" class="ml-2"
+              /></a>
+            </div>
+          </div>
+        </div>
+      </template>
+    </Transition>
 
     <!-- Connection failed -->
     <template v-if="errorState">
@@ -218,3 +220,14 @@ bus.on("open", () => {
     </template>
   </div>
 </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.2s;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
