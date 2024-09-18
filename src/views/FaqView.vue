df9c297f7e6a732cabf10daa9d1d8bb3db5688a0
alexiscolin <alexis@jaunebleu.co>
2024-09-19 03:38:58 +0900

    fix: max-w

--- src/views/FaqView.vue
+++ src/views/FaqView.vue
@@ -38,7 +38,7 @@ const md = markdownit({
     ></div>
 
     <div class="flex flex-row py-8 md:py-[72px]">
-      <p class="text-grey-100 text-400 lg:w-[px] font-normal text-left text-pretty">
+      <p class="text-grey-100 text-400 lg:w-[800px] font-normal text-left text-pretty">
         <span class="block pb-8 lg:pb-[72px]">
           <div v-for="(item, index) in faqContent" :key="index">
             <h2 class="text-light mb-6">{{ item.question }}</h2>
