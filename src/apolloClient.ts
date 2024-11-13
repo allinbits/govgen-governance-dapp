26831cfe215fd1e6a7571f041ff6e310c59a3b04
Alex M. - Clockwork <alex@clockwork.gr>
2024-11-13 16:31:22 +0200

    chore: update indexer URLs

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -3,7 +3,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client/core
 // HTTP connection to the API
 const httpLink = createHttpLink({
   // You should use an absolute URL here
-  uri: import.meta.env.VITE_GRAPHQL_ENDPOINT ?? "https://graphql-devnet.govgen.dev/v1/graphql",
+  uri: import.meta.env.VITE_GRAPHQL_ENDPOINT ?? "https://graphql-govgen-devnet-4.allinbits.services/v1/graphql",
 });
 
 // Cache implementation
